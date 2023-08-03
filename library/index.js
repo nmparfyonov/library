e1e9638ac843ca3d38025c454d71171aaa561fe8
nparfyonov <nikita.parfyonov@hf-solution.com>
2023-08-04 02:17:05 +0300

    feat: add hide menu on nav item click

--- library/index.js
+++ library/index.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerMenu = document.querySelector(".hamburger-menu");
     const navMenu = document.querySelector(".nav");
+    const navItem = document.querySelectorAll(".nav li");
 
     hamburgerMenu.addEventListener("click", () => {
         navMenu.classList.toggle("menu-active");
         hamburgerMenu.classList.toggle("menu-active");
+    });
+
+    navItem.forEach((x) => {
+        x.addEventListener("click", () => {
+            navMenu.classList.toggle("menu-active");
+            hamburgerMenu.classList.toggle("menu-active");
+        }
+        )
     });
 
 
