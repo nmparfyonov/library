4ee8cd4cd20754bdb1290198349b4ba0d056bfa6
nparfyonov <nikita.parfyonov@hf-solution.com>
2023-08-04 01:34:40 +0300

    feat: add functional burger

--- library/index.js
+++ library/index.js
@@ -1,1 +1,11 @@
-console.log('100 - все обязательные пункты сделаны')
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const navMenu = document.querySelector(".nav");
+
+    hamburgerMenu.addEventListener("click", () => {
+        navMenu.classList.toggle("menu-active");
+        hamburgerMenu.classList.toggle("menu-active");
+    });
+
+
+});
